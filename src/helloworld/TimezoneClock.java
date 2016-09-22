6f2b78cbcc11427cc550fb76839b49380718f4a3
austi <austi@65.126.170.82>
2016-09-22 11:51:11 -0600

    time zone clock

--- src/helloworld/TimezoneClock.java
+++ src/helloworld/TimezoneClock.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package helloworld;
 
 import static kiss.API.abs;
 import static kiss.API.pause;
 
-/**
- *
- * @author austi
- */
 public class TimezoneClock extends Clock {
 
     double timezoneShift = 0.0;
